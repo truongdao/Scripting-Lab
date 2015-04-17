7ac192d96a53c19264c2bfcf67ffeb5a0b6c1fc3
truongdao <doctdao@gmail.com>
2015-04-17 23:05:30 +0700

    added navigator functions: up, down, left, right... to Excel library.

--- excel.js
+++ excel.js
@@ -1,7 +1,6 @@
 importPackage(java.io);
-importClass(java.lang.Class);
-loadjar("D:/softs/java/poi/simplecodestuffs.com/poi-3.7-20101029.jar");
-importClass(Packages.org.apache.poi.hssf.usermodel.HSSFWorkbook);
+loadjar("./libs/poi-3.7-20101029.jar")
+importClass(Packages.org.apache.poi.hssf.usermodel.HSSFWorkbook)
 
 var Xls = {
 	path: ".",
@@ -17,19 +16,31 @@ var Book = {
 	sheets: null,		//get all sheets
 	cell: null,			//get a cell
 	cells: null,		// get all cells
-	save: null
+	save: null,			//save to current file
+	saveAs:	null		//save to new file
 };
 
 var Sheet = {
 	r : null,
+	book: null,
 	//APIs
+	left:	null,
+	right:	null,
 	cell: null,			//get a cell
 	cells: null			//get all cells to an array
 };
 
+
 var Cell = {
+	r:		null,
+	sheet:	null,
 	//APIs
-	r: null
+	up:		null,
+	down:	null,
+	left:	null,
+	right:	null,
+	empty:	null
+	
 };
 ////////////////////////// IMPLEMENTATIONS ////////////////////////////////////
 
@@ -52,8 +63,15 @@ Book.save = function(){
 	fos.close();
 };
 
+Book.saveAs = function(path){
+	var fos = new FileOutputStream(path);
+	this.r.write(fos);
+	fos.close();
+};
+
 Book.sheet = function(id){
 	var new_sheet = Object.create(Sheet);
+	new_sheet.book = this;
 	//if id is index
 	if(typeof id == 'number'){
 		new_sheet.r =  this.r.getSheetAt(id);
@@ -69,6 +87,7 @@ Book.sheets = function(){
 	var sheets = [];
 	for( var i=0; i< this.r.getNumberOfSheets(); i++){
 		var new_sheet = Object.create(Sheet);
+		new_sheet.book = this;
 		new_sheet.r =  this.r.getSheetAt(i);
 		sheets[i] = new_sheet;
 	}
@@ -90,6 +109,8 @@ Book.cells = function(sheet_id){
 
 Sheet.cell = function(row, col){
 	var new_cell = Object.create(Cell);
+	//mapping its parent
+	new_cell.sheet = this;
 	new_cell.r = this.r.getRow(row).getCell(col);
 	return new_cell;
 };
@@ -107,6 +128,8 @@ Sheet.cells = function(){
 		
 		while(cellIterator.hasNext()){
 			var new_cell = Object.create(Cell);
+			//mapping its parent
+			new_cell.sheet = this;
 			new_cell.r = cellIterator.next();
 			icells[icells.length] = new_cell;
 		}
@@ -115,4 +138,98 @@ Sheet.cells = function(){
 };
 
 
+/**
+ * Get a right Sheet from the current sheet an {@code offset} number of sheets.
+ *
+ * @param {@code offset} space in sheet unit.
+ * @return right sheet or {@code null}
+ */
+Sheet.right = function ( offset ) {
+	var right = Object.create(Sheet);
+	right.book = this.book;
+	try{
+		right.r = this.book.r.getSheetAt(
+					this.book.r["getSheetIndex(java.lang.String)"](this.r.getSheetName())
+				+	offset	);
+		return right;		
+	}catch(err){ return null; }
+};
 
+/**
+ * Get a left Sheet from the current sheet an {@code offset} number of sheets.
+ *
+ * @param {@code offset} space in sheet unit.
+ * @return left sheet or {@code null}
+ */
+Sheet.left = function ( offset ) {
+	return this.right( -offset);
+}
+//---------------------------------------------------------------------------//
+/**
+ * Get a Cell below the current Cell an {@code offset} number of rows.
+ *
+ * @param {@code offset} space in row unit.
+ * @return below cell or {@code null}
+ */
+Cell.down = function ( offset ){
+	var below = Object.create(Cell);
+	below.sheet = this.sheet;
+
+	try{
+		var below_row = this.sheet.r.getRow(this.r.getRowIndex() +  offset);
+		if(below_row !=null)
+			below.r = below_row.getCell(this.r.getColumnIndex());
+		else below.r = null;
+		
+	} catch(err){ below.r = null;}
+	return below;
+};
+
+/**
+ * Get a Cell on the right side the current Cell an {@code offset} number of rows.
+ *
+ * @param {@code offset} space in row unit. (>=0)
+ * @return right cell or {@code null}
+ */
+Cell.right = function ( offset ){
+	var cur_row = this.r.getRow();
+	var	icur_col = this.r.getColumnIndex();
+	var left_cell = Object.create(Cell);
+	left_cell.sheet = this.sheet;
+	try{
+		left_cell.r = cur_row.getCell(icur_col + offset);
+	} catch(err){ left_cell.r = null;}
+	return left_cell;
+};
+
+
+/**
+ * Get a Cell above the current Cell an {@code offset} number of rows.
+ *
+ * @param {@code offset} space in row unit.
+ * @return above cell or {@code null}
+ */
+Cell.up = function ( offset ){
+	return this.down(-offset);
+};
+
+/**
+ * Get a Cell on the left side the current Cell an {@code offset} number of rows.
+ *
+ * @param {@code offset} space in row unit. (>=0)
+ * @return left cell or {@code null}
+ */
+Cell.left = function ( offset ){
+	return this.right(-offset);
+};
+
+
+/**
+ * check if a Cell is not initialised or empty
+ */
+Cell.empty = function(){
+	if(this.r == null || this.r.toString().isEmpty())
+		return true;
+	else
+		return false;
+}
