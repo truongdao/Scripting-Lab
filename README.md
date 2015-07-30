109f65fa13ef3a0e7ab0532cbf4a4e10219210c1
truongdao <doctdao@gmail.com>
2015-07-30 22:55:05 +0700

    feature JRT report is added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Scripting-Lab
 A tool for running JavaScript in JVM with plugin gadgets for POI, Table. 
+just used in Java 7
+
--- ScriptingLab.data/config.js
+++ ScriptingLab.data/config.js
@@ -4,6 +4,7 @@ outln('Load cmd.js'); eval('ScriptingLab.data/cmd.js');
 outln('Load forms.js'); eval('ScriptingLab.data/forms.js');
 outln('Load excel.js'); eval('ScriptingLab.data/excel.js');
 outln('Load file.js'); eval('ScriptingLab.data/file.js');
+outln('Load report.js'); eval('ScriptingLab.data/report.js');
 
 outln('----- -------------------\n');
 
